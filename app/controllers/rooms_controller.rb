3bdfac1142b3c90f6fdd0649c89742325bbf8249
delaram <rostami.dela@gmail.com>
2017-11-03 21:27:19 +0000

    creation of Controll Room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,49 +1,50 @@
-class RoomController < ApplicationController
-
+class RoomsController < ApplicationController
+    
 before_action :set_room, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
-
+   
     def index
         @rooms = current_user.rooms
     end
-   
-   def new
+    
+    def new
         @room = current_user.rooms.build
-   end
-   
-   def create
-        @room = current_user.rooms.build(room_params)
-        if @room.save
-            redirect_to @room, notice:"Your listing has been added sucessfully"
+    end
+
+def create
+       @room = current_user.rooms.build(room_params)
+if @room.save
+           redirect_to @room, notice:"Votre annonce a été ajouté avec succès"
         else
             render :new
-    end
-   end
-   
+end
+end
+
     def show
     end
     
-     def edit
+    def edit
     end
     
-     def update
-         if @room.update(room_params)
-             redirect_to @room, notice:"Modification enregistrée..."
+    def update
+        if @room.update(room_params)
+            redirect_to @room, notice:"modification"
         else
-        render :edit        
+            render :edit
+        end
     end
-  end
-  
-  private
-    def set_room
+    
+private
+    def set_room    
         @room = Room.find(params[:id])
     end
-  
-   def room_params
-   params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, 
-   :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, 
-   :is_heating, :is_air, :is_kitchen, :price, :active)
-   end
-   
-   
+    
+    def room_params
+        params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room,:bath_room,
+        :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet,
+        :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
+    end
+    
+    
+    
 end
