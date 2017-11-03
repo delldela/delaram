166eca3fc91eb8ced0c324d84b92ad663cb21e70
delaram <rostami.dela@gmail.com>
2017-11-03 02:56:25 +0000

    new stuff added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,47 +1,49 @@
+class RoomController < ApplicationController
+
 before_action :set_room, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
-    
+
     def index
-    @rooms = current_user.rooms
+        @rooms = current_user.rooms
     end
-    
-    def new
+   
+   def new
         @room = current_user.rooms.build
-    end
-    
-    def create
+   end
+   
+   def create
         @room = current_user.rooms.build(room_params)
         if @room.save
-        redirect_to @room, notice:"Your adventure has been added successfully"
+            redirect_to @room, notice:"Your listing has been added sucessfully"
         else
-        render :new
+            render :new
     end
-end
-
-    
+   end
+   
     def show
     end
     
-    def edit
+     def edit
     end
     
-    def update
-        if @room.update(room_params)
-            redirect_to @room, notice:"Saved changes"
+     def update
+         if @room.update(room_params)
+             redirect_to @room, notice:"Modification enregistrée..."
         else
-            render :edit
-        end
+        render :edit        
     end
-    
-    private
-        def set_room
+  end
+  
+  private
+    def set_room
         @room = Room.find(params[:id])
     end
-    
-    def room_params
-        params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room,
-        :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
-        
-    end
-    
- end   
+  
+   def room_params
+   params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, 
+   :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, 
+   :is_heating, :is_air, :is_kitchen, :price, :active)
+   end
+   
+   
+end
