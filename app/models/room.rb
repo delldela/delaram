cf7e6d79e83a3ad75eb49fd9f8787cf9492b946b
delaram <rostami.dela@gmail.com>
2017-11-15 21:29:57 +0000

    review write up

--- app/models/room.rb
+++ app/models/room.rb
@@ -27,8 +27,6 @@ class Room < ActiveRecord::Base
                after_validation :geocode, if: :address_changed?
                
                def average_rating
- 
-        reviews.count == 0 ? 0 : reviews.average(:star).round(2)
- 
- end
+                reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+               end
 end
